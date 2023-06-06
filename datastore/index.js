de793fcca183fc1bf87fd25e2ffbc3e1be01aef1
Mead Solomon <mead93@gmail.com>
2023-06-06 14:32:42 -0500

    complete updating and deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -111,24 +111,51 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  const filePath = path.join(exports.dataDir, `${id}.txt`);
+
+  exports.readOne(id, (err) => {
+    if (err) {
+      callback(err)
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, text);
+        }
+      })
+    }
+  })
+
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
   var item = items[id];
   delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  const filePath = path.join(exports.dataDir, `${id}.txt`);
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
+
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
